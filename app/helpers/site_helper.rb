fbc74656cbb1671cf63fb3eece852ffe6a37e0aa
Dimitri Kurashvili <dimitri@c12.ge>
2012-03-19 12:11:31 +0400

    main menu

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,15 @@
+require 'yaml'
+
 module SiteHelper
+
+  def application_menu
+    menu = YAML.load_file('config/main_menu.yml')
+    menu = menu['not_authorized']
+    main_menu = menu['main']
+    secondary_menu = menu['secondary']
+    page = request.fullpath[1..-1]
+    page = 'home' if page.empty?
+    render :partial => 'layouts/main_menu', :locals => {:items => main_menu, :secondary_items => secondary_menu, :page => page}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 TelasiService::Application.routes.draw do
-  get "site/index"
+  get 'site/index'
+
+  get '/home', :controller => :site, :action => :index, :as => :home
+  match '/register', :controller => :site, :action => :register, :as => :register
+  match '/login', :controller => :site, :action => :login, :as => :login
 
   root :to => 'site#index'
 end
