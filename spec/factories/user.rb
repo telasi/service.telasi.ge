74d4cb5bea3d1c2ec4129c9381c4c12bf638c5cb
Dimitri Kurashvili <dimitri@c12.ge>
2012-03-19 21:27:32 +0400

    user factory

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+# -*- encoding : utf-8 -*-
+FactoryGirl.define do
+  factory :user do
+    email      'dimitri@c12.ge'
+    password   'secret'
+    mobile     '595335514'
+    first_name 'Dimitri'
+    last_name  'Kurashvili'
+  end
+end
