ca68fbf2e5143fb25c8753de14869939706a6a9e
Dimitri Kurashvili <dimitri@c12.ge>
2012-03-23 08:49:13 +0400

    part I of the app PDF complete

--- lib/telasi/app_print.rb
+++ lib/telasi/app_print.rb
@@ -11,6 +11,7 @@ module Telasi
   private
 
   DEF_FONT_SIZE = 10
+  DATA_SIZE = DEF_FONT_SIZE - 1
   SMALL_SIZE = 5
 
   def self.build_pdf(app, pdf)
@@ -37,7 +38,7 @@ module Telasi
     pdf.change_font(:default, DEF_FONT_SIZE)
     d = C12::KA.format_date(Time.now)
     pdf.table [['განაცხადის შევსების თარიღი', d]], :column_widths => [200, 150], :cell_style => {:borders => []} do
-      column(1).style(:align => :center, :borders => [:bottom])
+      column(1).style(:align => :center, :borders => [:bottom], :size => DATA_SIZE)
     end
   end
 
@@ -56,7 +57,7 @@ module Telasi
     ]
     pdf.table items, :column_widths => [250] do
       column(0).style(:borders => [])
-      column(1).style(:borders => [:bottom], :align => :center, :padding => [10, 0, 5, 0])
+      column(1).style(:borders => [:bottom], :align => :center, :padding => [10, 0, 5, 0], :size => DATA_SIZE)
       row(2).style(:borders => [], :size => SMALL_SIZE, :padding => 2)
     end
 
@@ -66,7 +67,37 @@ module Telasi
     items = [['მისამართი', app.applicant.address]]
     pdf.table items, :column_widths => [100, pdf.bounds.width - 100] do
       column(0).style(:borders => [])
-      column(1).style(:borders => [:bottom], :align => :center)
+      column(1).style(:borders => [:bottom], :align => :center, :size => DATA_SIZE)
+    end
+
+    pdf.move_down 10
+    items = [['მობ. ტელ.', app.applicant.mobile, 'ელ. ფოსტა', app.applicant.email]]
+    pdf.table items, :column_widths => [75, 120, 75, pdf.bounds.width - 270], :cell_style => {:borders => []} do
+      column(1).style(:borders => [:bottom], :align => :center, :size => DATA_SIZE)
+      column(2).style(:align => :center)
+      column(3).style(:borders => [:bottom], :align => :center, :size => DATA_SIZE)
+    end
+
+    pdf.move_down 10
+    items = [['მიერთების მსურველის (განმცხადებლის) საბანკო რეკვიზიტები:'], ["#{app.bank_account.swift} #{app.bank_account.bank} #{app.bank_account.eban}".strip]]
+    pdf.table items, :column_widths => [pdf.bounds.width] do
+      row(0).style(:borders => [])
+      row(1).style(:borders => [:bottom], :size => DATA_SIZE, :align => :center)
+    end
+
+    pdf.move_down 10
+    items = [['ადგილი (მისამართი) სადაც უნდა მოხდეს ელექტრომომარაგება'], [app.address]]
+    pdf.table items, :column_widths => [pdf.bounds.width] do
+      row(0).style(:borders => [])
+      row(1).style(:borders => [:bottom], :size => DATA_SIZE, :align => :center)
+    end
+
+    pdf.move_down 10
+    email = "#{app.email_response ? '☑' : '☐' } ელექტრონული"
+    writn = "#{app.written_response ? '☑' : '☐' } წერილობითი"
+    items = [['განაწილების ლიცენზიატის მიერ შეტყობინების გაგზავნის ფორმა:'], ["#{writn}; #{email}"]]
+    pdf.table items, :cell_style => {:borders => []}, :column_widths => [pdf.bounds.width] do
+      row(1).style(:align => :center)
     end
   end
 
