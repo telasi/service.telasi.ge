5f944b9d7c5d5d49ca65100639fbd517f1da7e70
Dimitri Kurashvili <dimitri@c12.ge>
2012-04-26 15:52:19 +0400

    adding users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+# -*- encoding : utf-8 -*-
+class UsersController < ApplicationController
+
+  # მომხმარებლის ავტორიზაციის გვერდი.
+  def login
+    @title = 'სისტემაში შესვლა'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,17 @@ TelasiService::Application.routes.draw do
   end
 
   # user actions
-  match '/register', :controller => :site, :action => :register, :as => :register
-  match '/login', :controller => :site, :action => :login, :as => :login
-  get '/logout', :controller => :site, :action => :logout, :as => :logout
-  get '/confirm', :controller => :site, :action => :confirm, :as => :confirm
-  match '/account', :controller => :site, :action => :account, :as => :account
-  match '/change_password', :controller => :site, :action => :change_password, :as => :change_password
+  scope '/user', :controller => :users do
+    match '/register', :action => :register
+    match '/login', :action => :login
+  end
+
+#  match '/register', :controller => :site, :action => :register, :as => :register
+#  match '/login', :controller => :site, :action => :login, :as => :login
+#  get '/logout', :controller => :site, :action => :logout, :as => :logout
+#  get '/confirm', :controller => :site, :action => :confirm, :as => :confirm
+#  match '/account', :controller => :site, :action => :account, :as => :account
+#  match '/change_password', :controller => :site, :action => :change_password, :as => :change_password
 
 #  # application actions
 #  scope '/app', :controller => :applications do
