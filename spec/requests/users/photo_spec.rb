91cc8c7a9a350ea0bf3d978bf9e2fe4ee55d0488
Dimitri Kurashvili <dimitri@c12.ge>
2012-04-30 15:02:00 +0400

    change profile picture spec

---
+++ spec/requests/users/photo_spec.rb
@@ -0,0 +1,24 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+feature 'მომხმარებლის გამოსახულების შეცვლა' do
+  before(:all) do
+    login('dimitri@c12.ge', 'secret', :create => true)
+  end
+  context 'გამოსახულების შეცვლამდე' do
+    subject { User.first }
+    its(:photo) { should_not be_file }
+  end
+  context 'გამოსახულების შეცვლა' do
+    before(:all) do
+      login('dimitri@c12.ge', 'secret')
+      visit user_photo_url
+      attach_file 'user_photo', File.join(Rails.root, 'spec/assets/images/user_photo.jpg')
+      click_button 'გამოსახულების შეცვლა'
+    end
+    subject { User.where(:email => 'dimitri@c12.ge').first }
+    its(:photo) { should be_file }
+    specify { subject.photo.original_filename.should == 'user_photo.jpg' }
+  end
+end
+
