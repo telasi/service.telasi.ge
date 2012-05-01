42e1c27487aa9602dc227aedd520ebce8aa8f514
Dimitri Kurashvili <dimitri@c12.ge>
2012-05-01 14:53:09 +0400

    applications home page

---
+++ app/controllers/apps/applications_controller.rb
@@ -0,0 +1,6 @@
+# -*- encoding : utf-8 -*-
+class Apps::ApplicationsController < ApplicationController
+	def index
+		@title = 'title'
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -23,20 +23,12 @@ TelasiService::Application.routes.draw do
     match '/photo',           :action => :photo,          :as => :user_photo
   end
 
-#  # application actions
-#  scope '/app', :controller => :applications do
-#    get '/tariffs', :action => :tariffs, :as => :tariffs
-#    match '/new', :action => :new, :as => :new_application
-#    get '/show/:id', :action => :show, :as => :show_application
-#    get '/print/:id', :action => :print, :as => :print_application
-#    match '/edit/:id', :action => :edit, :as => :edit_application
-#    delete '/delete/:id', :action => :delete, :as => :delete_application
-#    scope '/item' do
-#      match '/new/:id', :action => :new_item, :as => :new_application_item
-#      match '/edit/:app_id/:id', :action => :edit_item, :as => :edit_application_item
-#      delete '/delete/:app_id/:id', :action => :delete_item, :as => :delete_application_item
-#    end
-#  end
+  # application actions
+  namespace :apps do
+  	scope :controller => :applications do
+  		get '/', :action => :index, :as => :home
+  	end
+  end
 
   root :to => 'site#index'
 end
