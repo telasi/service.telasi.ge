dbcf74918959a4ea4549667655ed21ad26bd5374
Dimitri Kurashvili <dimitri@c12.ge>
2012-05-04 17:51:07 +0400

    sync

--- app/controllers/apps/new_customer_controller.rb
+++ app/controllers/apps/new_customer_controller.rb
@@ -1,48 +1,47 @@
 # -*- encoding : utf-8 -*-
 class Apps::NewCustomerController < ApplicationController
 
-	def new
-		@title = 'ქსელზე მიერთების ახალი განცხადება'
-		if request.post?
-			@application = Apps::Application.new(params[:apps_application])
-			@application.applicant = Apps::Applicant.new(params[:apps_applicant])
-			@application.new_customer_application = Apps::NewCustomerApplication.new(params[:apps_new_customer_application])
-			@application.owner = current_user
-			@application.type = Apps::Application::TYPE_NEW_CUSTOMER
-			redirect_to apps_new_customer_path(:id => @application.id), :notice => 'განცხადება შექმნილია.' if @application.save
-		else
-			applicant = Apps::Applicant.new(:mobile => current_user.mobile, :email => current_user.email)
+  def new
+    @title = 'ქსელზე მიერთება: ახალი განცხადება'
+    if request.post?
+      @application = Apps::Application.new(params[:apps_application])
+      @application.applicant = Apps::Applicant.new(params[:apps_applicant])
+      @application.new_customer_application = Apps::NewCustomerApplication.new(params[:apps_new_customer_application])
+      @application.owner = current_user
+      @application.type = Apps::Application::TYPE_NEW_CUSTOMER
+      redirect_to apps_new_customer_path(:id => @application.id), :notice => 'განცხადება შექმნილია.' if @application.save
+    else
+      applicant = Apps::Applicant.new(:mobile => current_user.mobile, :email => current_user.email)
       new_customer_application = Apps::NewCustomerApplication.new(:voltage => Apps::NewCustomerApplication::VOLTAGE_220)
-			@application = Apps::Application.new(:applicant => applicant, :new_customer_application => new_customer_application)
-		end
-	end
+      @application = Apps::Application.new(:applicant => applicant, :new_customer_application => new_customer_application)
+    end
+  end
 
-	def show
-	  @title = 'განცხადების დეტალები'
+  def show
+    @title = 'განცხადების დეტალები'
     @application = Apps::Application.where(:_id => params[:id]).first
     # XXX
-	end
+  end
 
-	def customers
-	  @title = 'აბონენტები'
+  def customers
+    @title = 'აბონენტები'
     @application = Apps::Application.where(:_id => params[:id]).first
-    # XXX
-	end
+  end
 
-	def notes
+  def notes
     @title = 'შენიშვნები'
     @application = Apps::Application.where(:_id => params[:id]).first
-	end
+  end
 
-	def docs
+  def docs
     @title = 'დოკუმენტები'
     @application = Apps::Application.where(:_id => params[:id]).first
-	end
+  end
 
-	def delete
+  def delete
     @application = Apps::Application.where(:_id => params[:id]).first
     @application.destroy
     redirect_to apps_home_path, :notice => 'განცხადება წაშლილია.'
-	end
+  end
 
 end
