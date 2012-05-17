35d2db9a4179be22fa839eab2638571ef5658215
Dimitri Kurashvili <dimitri@c12.ge>
2012-05-17 16:45:40 +0400

    role_content helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,23 @@ module ApplicationHelper
   def current_user
     controller.current_user
   end
+
+  def role_content(*role, &block)
+    user = current_user
+    capture &block if user and with_role?(user, role)
+  end
+
+  # აქვს თუ არა მომხმარებელს არგუმენტში მოცემული როლი.
+  # როლი შეიძლება გადმოსცეთ მასივის ან ცალკეული სიმბოლოს სახით.
+  def with_role?(user, role)
+    if role.is_a? Array
+      role.each { |r| return true if with_role?(employee, r) }
+      false
+    elsif role.to_sym == :all
+      true
+    else
+      employee.send role.to_sym if employee.respond_to?(role.to_sym)
+    end
+  end
+
 end
