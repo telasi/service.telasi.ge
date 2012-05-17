8c35f9d8ad85f6f5d040e5ad53203902f1ed119d
Dimitri Kurashvili <dimitri@c12.ge>
2012-05-17 18:18:46 +0400

    first version of users page

--- app/controllers/sys/users_controller.rb
+++ app/controllers/sys/users_controller.rb
@@ -4,6 +4,7 @@ class Sys::UsersController < ApplicationController
   # მომხმარებლების საწყისი გვერდი.
   def index
     @title = 'მომხმარებლები'
+    @users = User.desc(:_id).paginate(:per_page => 5)
   end
 
 end
