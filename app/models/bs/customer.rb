3216b9db470ea67028c2f50ce7e933b9444ad60c
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-13 16:07:19 +0400

    adding customer address

--- app/models/bs/customer.rb
+++ app/models/bs/customer.rb
@@ -3,4 +3,5 @@ class Bs::Customer < ActiveRecord::Base
   self.table_name  = 'customer'
   self.primary_key = :custkey
   has_one :trash_customer, class_name: 'Bs::TrashCustomer', foreign_key: :custkey
+  has_one :address, class_name: 'Bs::Address', foreign_key: :premisekey
 end
---
+++ app/models/bs/street.rb
@@ -0,0 +1,5 @@
+# -*- encoding : utf-8 -*-
+class Bs::Street < ActiveRecord::Base
+  set_table_name 'street'
+  set_primary_key :streetkey
+end
