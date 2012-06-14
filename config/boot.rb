9b940f2df5c50edd074d6c82754b943743626228
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-14 09:53:33 +0400

    oracle config

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,9 @@
 # -*- encoding : utf-8 -*-
 require 'rubygems'
 
+# Set oracle's NLS_LANG parameter.
+ENV['NLS_LANG'] = 'AMERICAN_AMERICA.UTF8'
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
