73a88768e380b5d7f55437ceb928c25c71916d53
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-21 14:26:20 +0400

    map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,1 +1,10 @@
 # -*- encoding : utf-8 -*-
+class MapController < ApplicationController
+
+  # რუკა.
+  def index
+    @title = 'ბიზნეს-ცენტრების რუკა'
+    @regions = Ext::Region.where(show_on_map: true).asc(:regionkey)
+  end
+
+end
