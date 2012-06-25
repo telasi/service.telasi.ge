7b687fe2599b8a4a633d9c5ff4cfbbce860ec4c4
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-25 11:06:53 +0400

    display balance column

--- app/models/bs/item.rb
+++ app/models/bs/item.rb
@@ -5,4 +5,19 @@ class Bs::Item < ActiveRecord::Base
   belongs_to :customer,  class_name: 'Bs::Customer', foreign_key: :custkey
   belongs_to :account,   class_name: 'Bs::Account',  foreign_key: :acckey
   belongs_to :operation, class_name: 'Bs::Billoperation', foreign_key: :billoperkey
+
+  # ოპერაციის "ნორმალიზუბული" მნიშვნელობა.
+  def normal_amount
+    if self.operation.opertpkey == 3
+      - self.amount
+    else
+      self.amount
+    end
+  end
+
+  # ოპერაციის ნორმალიზებული ბალანსი.
+  def normal_balance
+    self.balance + self.normal_amount
+  end
+
 end
