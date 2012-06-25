54a32e234892bda5b819cea66f851c14bc2a2908
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-25 18:17:46 +0400

    some item pages

--- app/models/bs/billoperation.rb
+++ app/models/bs/billoperation.rb
@@ -2,4 +2,8 @@
 class Bs::Billoperation < ActiveRecord::Base
   self.table_name  = 'bs.billoperation'
   self.primary_key = :billoperkey
+
+  def ext_operation
+    Ext::Billoperation.where(billoperkey: self.billoperkey).first
+  end
 end
