9d0212ed5c966c3f3f60eda01e7a79104692abeb
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-26 12:35:06 +0400

    more detailed trash forms

--- app/models/bs/trash_item.rb
+++ app/models/bs/trash_item.rb
@@ -4,4 +4,18 @@ class Bs::TrashItem < ActiveRecord::Base
   self.primary_key = :trashitemkey
   belongs_to :customer,  class_name: 'Bs::Customer', foreign_key: :custkey
   belongs_to :operation, class_name: 'Bs::Trashoperation', foreign_key: :operationid
+
+  # ოპერაციის "ნორმალიზუბული" მნიშვნელობა.
+  def normal_amount
+    if self.operation.opergroupid == 2
+      - self.amount
+    else
+      self.amount
+    end
+  end
+
+  def normal_balance
+    self.balance - self.normal_amount
+  end
+
 end
