f3ceb2cde6d2704d5f938676014edc0b5550c2f0
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-27 14:59:27 +0400

    filter transformators

--- app/controllers/sys/gis_controller.rb
+++ app/controllers/sys/gis_controller.rb
@@ -3,12 +3,18 @@ class Sys::GisController < ApplicationController
 
   def transformators
     @title = 'ტრანსფორმატორები'
-    @transformators = Ext::Transformator.asc(:tp_name, :tr_name)
+    rel = Ext::Transformator
+    rel = rel.where(acckey: nil) if params[:filter] == 'problem'
+    @transformators = rel.asc(:tp_name, :tr_name)
   end
 
   def sync_transformators
     Ext::Transformator.sync
     redirect_to sys_transformators_url, notice: 'სინქრონიზაცია დასრულებულია.'
+  end
+
+  def logs
+    @title = 'ლოგები'
   end
 
 end
