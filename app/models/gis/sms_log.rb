09f5e0ebddbb3995e9cbe7c43a268dddce9b4afc
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-28 15:09:01 +0400

    fixing gis models

--- app/models/gis/sms_log.rb
+++ app/models/gis/sms_log.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Gis::SmsLog < ActiveRecord::Base
-  self.inheritance_column = nil if Rails.env.development?
+  self.inheritance_column = nil
   self.table_name = 'mv_dispetch.sms_log'
   self.primary_key = :sms_log_id
 
