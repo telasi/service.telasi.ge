46e0072642961ae4401a724a235d146b4476afb5
Dimitri Kurashvili <dimitri@c12.ge>
2012-09-03 18:04:41 +0400

    getting parameters from the request

--- app/controllers/android/readings_controller.rb
+++ app/controllers/android/readings_controller.rb
@@ -2,10 +2,10 @@
 class Android::ReadingsController < ApplicationController
 
   def reester
-    date = Date.strptime '1-Jun-2012', '%d-%b-%Y'
-    inspector = 11795
-    #date = Date.strptime params[:date], '%d-%b-%Y' unless params[:date].blank?
-    #inspector = params[:inspectorid]
+    #date = Date.strptime '1-Jun-2012', '%d-%b-%Y'
+    #inspector = 11795
+    date = Date.strptime params[:date], '%d-%b-%Y' unless params[:date].blank?
+    inspector = params[:inspector]
     @route = Bs::RouteStoreHeader.where(cycledate: date, inspectorid: inspector).first
   end
 
