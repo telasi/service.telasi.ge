18281a89097a346d40c0e74181c3212b653c2fc9
Dimitri Kurashvili <dimitri@c12.ge>
2012-09-18 11:26:26 +0400

    status IN (0,1) in route store are acceptable

--- app/models/bs/route_store_header.rb
+++ app/models/bs/route_store_header.rb
@@ -7,6 +7,6 @@ class Bs::RouteStoreHeader < ActiveRecord::Base
   self.table_name  = 'bs.route_store_header'
   self.primary_key = :route_header_id
   has_many :all_items, class_name: 'Bs::RouteStoreItem', foreign_key: :route_header_id, order: :read_seq
-  has_many :items, class_name: 'Bs::RouteStoreItem', foreign_key: :route_header_id, order: :read_seq, conditions: {status: 1}
+  has_many :items, class_name: 'Bs::RouteStoreItem', foreign_key: :route_header_id, order: :read_seq, conditions: {status: [0,1]}
   belongs_to :inspector, class_name: 'Bs::Person', foreign_key: :inspectorid
 end
