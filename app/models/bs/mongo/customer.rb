87e41e72fe81341f9bdfd1557a55d2fd71d92943
Dimitri Kurashvili <dimitri@c12.ge>
2013-02-28 11:09:20 +0400

    adding customer class (for Mongoid)

---
+++ app/models/bs/mongo/customer.rb
@@ -0,0 +1,53 @@
+# -*- encoding : utf-8 -*-
+class Bs::Mongo::Customer
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :custkey, type: Integer
+  embeds_one :address, class_name: 'Bs::Mongo::Address'
+  embeds_one :send_address, class_name: 'Bs::Mongo::Address'
+  embeds_one :category, class_name: 'Bs::Mongo::CustomerCategory'
+  embeds_one :activity, class_name: 'Bs::Mongo::CustomerCategory'
+  field :note, type: String
+  field :accnumb, type: String
+  field :custname, type: String
+  field :comercial, type: String
+  field :createdate, type: Date
+  field :closedate, type: Date
+  field :tel, type: String
+  field :fax, type: String
+  field :email, type: String
+  field :taxid, type: String
+  field :contact, type: String
+  field :balance, type: Float
+  field :old_balance, type: Float
+  field :illegalline, type: Integer
+  field :except, type: Integer
+  field :goodpayer, type: Integer
+  field :payint, type: Integer
+  field :cut, type: Integer
+  field :status, type: Integer
+
+end
+
+class Bs::Mongo::Address
+  include Mongoid::Document
+  field :premisekey, type: Integer
+  field :regionkey,  type: Integer
+  field :regionname, type: String
+  field :streetkey,  type: Integer
+  field :streetname, type: String
+  field :house,    type: String
+  field :building, type: String
+  field :porch,    type: String
+  field :flate,    type: String
+  field :postindex,  type: String
+  field :roomnumber, type: Integer
+  field :read_seq,   type: Integer
+end
+
+class Bs::Mongo::CustomerCategory
+  include Mongoid::Document
+  field :custcatkey, type: Integer
+  field :custcatname, type: String
+end
---
+++ spec/model/bs/customer_spec.rb
@@ -0,0 +1,6 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe Bs::Mongo::CustomerCategory do
+  
+end
