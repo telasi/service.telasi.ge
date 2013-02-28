0ce5d77bee3790dfc3a74a1194607eed15f7b025
Dimitri Kurashvili <dimitri@c12.ge>
2013-02-28 16:41:24 +0400

    adding customer notes

--- app/models/bs/customer.rb
+++ app/models/bs/customer.rb
@@ -8,6 +8,7 @@ class Bs::Customer < ActiveRecord::Base
   has_many :water_items,    class_name: 'Bs::WaterItem',     foreign_key: :custkey, order: 'year, month'
   has_many :item_bills,     class_name: 'Bs::ItemBill',      foreign_key: :custkey, order: 'itemkey'
   has_many :accounts,       class_name: 'Bs::Account',       foreign_key: :custkey
+  has_one  :note,           class_name: 'Bs::Note',          foreign_key: :notekey
 
   def pre_payment
     Bs::Payment.where('paydate > ? AND custkey = ? AND status = 1', Date.today - 7, self.custkey).inject(0) do |sum, payment|
