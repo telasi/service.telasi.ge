4e8eccf5403a6ca618c9456268323cacaa97463c
Dimitri Kurashvili <dimitri@c12.ge>
2013-03-01 16:22:46 +0400

    cut history

--- app/models/bs/cut_history.rb
+++ app/models/bs/cut_history.rb
@@ -2,14 +2,28 @@
 class Bs::CutHistory < ActiveRecord::Base
   self.table_name  = 'dimitri.ruby_cut_history'
   self.primary_key = :cr_key
+  set_integer_columns :oper_code, :mark_code
   belongs_to :customer,  class_name: 'Bs::Customer', foreign_key: :custkey
   belongs_to :account,   class_name: 'Bs::Account',  foreign_key: :acckey
 
+  def restore?
+    self.oper_code == 1
+  end
+
+  def complete?
+    self.mark_code == 1
+  end
+
   def operation
-    if oper_code
-      'აღდგენა'
+    self.restore? ? 'აღდგენა' : 'ჩაჭრა'
+  end
+
+  def result
+    if self.restore?
+      self.complete? ? 'აბონენტი აღდგენილია' : 'აბონენტი ვერ აღდგა'
     else
-      'ჩაჭრა'
+      self.complete? ? 'აბონენტი ჩაჭრილია' : 'აბონენტი არ ჩაჭრილა'
     end
   end
+
 end
