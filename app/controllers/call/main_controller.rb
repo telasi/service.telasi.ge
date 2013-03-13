2773a45556905709e9b1178b256fad50a5aa5533
Dimitri Kurashvili <dimitri@c12.ge>
2013-03-13 18:29:34 +0400

    show uncomplete tasks on start page

--- app/controllers/call/main_controller.rb
+++ app/controllers/call/main_controller.rb
@@ -3,6 +3,10 @@ class Call::MainController < ApplicationController
 
   def index
     @title = 'ქოლ-ცენტრი'
+    @tasks = Call::Task.where(complete: false).desc(:_id).paginate(per_page: 10, page: params[:page])
+    @task_table = TaskForm.task_table(@tasks)
+    @task_table.item_actions.clear
+    @task_table.title = 'შეუსრულებელი დავალებები'
   end
 
 end
