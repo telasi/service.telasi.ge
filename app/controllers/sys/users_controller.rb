453666a5ab6264e45ede66eb8efc64746be0c6b0
Dimitri Kurashvili <dimitri@c12.ge>
2013-03-26 18:32:03 +0400

    10 users per page

--- app/controllers/sys/users_controller.rb
+++ app/controllers/sys/users_controller.rb
@@ -5,7 +5,7 @@ class Sys::UsersController < ApplicationController
   def index
     @q = params[:q]
     @title = 'მომხმარებლები'
-    @users = User.by_q(@q).desc(:created_at).paginate(page: params[:page], per_page: 5)
+    @users = User.by_q(@q).desc(:created_at).paginate(page: params[:page], per_page: 10)
     render :json => {:q => @q, :d => render_to_string(:partial => 'sys/users/list')} if request.xhr?
   end
 
