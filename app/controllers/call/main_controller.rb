4460e1688269345cb3b4804de5f4b7da705a4430
Dimitri Kurashvili <dimitri@c12.ge>
2013-04-16 10:02:10 +0400

    removing favorites

--- app/controllers/call/main_controller.rb
+++ app/controllers/call/main_controller.rb
@@ -6,7 +6,7 @@ class Call::MainController < ApplicationController
   def index
     @title = 'ქოლ-ცენტრი'
     @tasks = open_tasks.desc(:_id).paginate(per_page: 10, page: params[:page])
-    @favorites = Call::Task.by_user(current_user).where(:_id.in => (current_user.favorite_task_ids || [])).desc(:_id)
+    #@favorites = Call::Task.by_user(current_user).where(:_id.in => (current_user.favorite_task_ids || [])).desc(:_id)
     @stats = Call::Status.asc(:order_by)
     @mobiles = Call::RegionData.asc(:_id)
   end
