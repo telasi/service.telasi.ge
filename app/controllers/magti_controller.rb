552f93420239f4d82ff25bc35df31b66eed58697
Dimitri Kurashvili <dimitri@c12.ge>
2013-04-17 15:34:08 +0400

    process magti messages

--- app/controllers/magti_controller.rb
+++ app/controllers/magti_controller.rb
@@ -2,8 +2,34 @@
 class MagtiController < ApplicationController
 
   def index
-    Magti.send_sms "595335514", "SMS migebulia"
-    render layout: false
+    msg_90033(params[:from], params[:text]) if params[:id] == '90033'
+    render text: find_accnumb_in_text(params[:text])
+  end
+
+  private
+
+  # message @90033
+  def msg_90033(from, text)
+    accnumb = find_accnumb_in_text(text) rescue nil
+    if accnumb
+      customer = Bs::Customer.where(accnumb: accnumb).first
+      user = User.where(sys_admin: true).first
+      stat = Call::Status.where(complete: true).first
+      open_ids = Call::Status.where(open: true).each.map{ |s| s.id }
+      Call::Task.where(custkey: customer.custkey, :status_id.in => open_ids).each do |t|
+        if t.status != stat
+          Call::TaskComment.new(text: "[SMS: #{from}] დავალება შესრულებულია", task: t, user: user).save
+          t.status = stat
+          t.save
+        end
+      end
+    end
+  end
+
+  def find_accnumb_in_text(text)
+    indx1 = text.index('abon: ') + 6
+    indx2 = text[indx1..-1].index(' ') + indx1 - 1
+    text[indx1..indx2]
   end
 
 end
