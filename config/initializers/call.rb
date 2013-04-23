628876a8c8822966bac4d285fd2818a72618f83b
Dimitri Kurashvili <dimitri@c12.ge>
2013-04-23 10:17:50 +0400

    new SMS schedule

--- config/initializers/call.rb
+++ config/initializers/call.rb
@@ -10,7 +10,12 @@ module Call
 
   def self.call_center_sms_send_time
     hour = Time.now.localtime.hour
-    (hour >= 18 && hour <= 21) # 18:00:00 - 21:59::59
+    wday = Time.now.localtime.wday
+    if wday == 6 or wday == 7
+      (hour >= 10 && hour <= 21) # Weekends 10:00:00 - 21:59::59
+    else
+      (hour >= 18 && hour <= 21) # Weekdays 18:00:00 - 21:59::59
+    end
   end
 
 end
