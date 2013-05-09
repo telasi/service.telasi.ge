50a7263a122fd08d8d7ca28a0d1e6ade81270c3e
Dimitri Kurashvili <dimakura@gmail.com>
2013-05-09 13:52:41 +0400

    Update call.rb

--- config/initializers/call.rb
+++ config/initializers/call.rb
@@ -1,21 +1,21 @@
 # -*- encoding : utf-8 -*-
-
 module Call
-
-  # ეს ინტერვალი გამოიყენება დავალებების დასასინქონებლად.
   SYNC = 100.hours
-
-  # ამ ინტერვალში შეიძლება მაქსიმუმ 1 დავალების გაშვება.
   REPEAT = 30.minutes
+  WEEKDAYS = [ Date.new(2013,5,9) ]
+
+  def self.is_weekend(time)
+    wday = time.wday
+    wday == 6 or wday == 7 or WEEKDAYS.include?(time.to_date)
+  end
 
   def self.call_center_sms_send_time
     hour = Time.now.localtime.hour
-    wday = Time.now.localtime.wday
-    if wday == 6 or wday == 7
+    #wday = Time.now.localtime.wday
+    if is_weekend(Time.now.localtime)
       (hour >= 10 && hour <= 21) # Weekends 10:00:00 - 21:59::59
     else
       (hour >= 18 && hour <= 21) # Weekdays 18:00:00 - 21:59::59
     end
   end
-
 end
