79f49cb89f9ae987aea94bbdee2b23cd08cb5103
Dimitri Kurashvili <dimakura@gmail.com>
2013-09-13 00:07:46 -0700

    Update water_customer_form.rb

--- app/forms/water_customer_form.rb
+++ app/forms/water_customer_form.rb
@@ -4,7 +4,7 @@ module WaterCustomerForm
 
   def self.customer_form(cust)
     f1 = NumberField.new(name: 'pre_water_payment', after: 'GEL')
-    f2 = DateField.new(name: 'pre_water_payment_date', formatter: '%d-%b-%Y %H:%M:%S')
+    f2 = DateField.new(name: 'pre_water_payment_date', formatter: '%d-%b-%Y %H:%M:%S', not_local: true)
     pre = ComplexField.new(fields: [f1, f2], label: 'დაუდ.გადახდა')
     form = Form.new(title: 'წყალმომარაგების აბონენტი')
     form.col1 << NumberField.new(name: 'water_balance', label: 'სრული ვალი', after: 'GEL', required: true)
