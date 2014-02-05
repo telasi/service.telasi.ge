f606b68d13541bacf4fed93f801e23cd6ac538e4
Dimitri Kurashvili <dimakura@gmail.com>
2014-02-05 10:52:54 +0400

    fixing water payment

--- app/models/bs/water_payment.rb
+++ app/models/bs/water_payment.rb
@@ -1,8 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Bs::WaterPayment < ActiveRecord::Base
-  #self.table_name  = 'paymentsadmin.payments_gwp'
-  #self.primary_key = :opayment_id
-  self.table_name  = 'bs.ruby_water_payments_didube'
+  self.table_name  = 'payments.water_payment'
   self.primary_key = :paykey
   belongs_to :customer, class_name: 'Bs::Customer', foreign_key: :custkey
 end
