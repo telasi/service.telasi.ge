8c2eddafb3e410fb5546b5d27790dd4b9562b1c4
Dimitri Kurashvili <dimakura@gmail.com>
2014-05-22 17:36:01 +0400

    7days->10days

--- app/controllers/android/readings_controller.rb
+++ app/controllers/android/readings_controller.rb
@@ -18,7 +18,7 @@ class Android::ReadingsController < ApplicationController
         @region = Bs::Region.find(params[:regionkey])
         rel = rel.joins(:route => :block).where('block.regionkey=?', params[:regionkey])
       end
-      rel=rel.where('cycledate>?', Date.today-7)
+      rel=rel.where('cycledate>?', Date.today-10)
       @routes = rel.paginate(per_page: 30, page: params[:page]).order('route_header_id DESC')
     end
   end
