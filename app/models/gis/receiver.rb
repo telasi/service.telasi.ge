f88e44cc7cb84c58d46f52d37f4cec26ac628e7d
Dimitri Kurashvili <dimakura@gmail.com>
2014-05-22 11:08:48 +0400

    don't translate russian locale to english in SMS messages

--- app/models/gis/receiver.rb
+++ app/models/gis/receiver.rb
@@ -16,7 +16,8 @@ class Gis::Receiver
     Gis::Receiver.where(active: true).each do |r|
       mobile = msg.on ? r.mobile_on : r.mobile_off
       email  = msg.on ? r.email_on  : r.email_off
-      Magti.send_sms(mobile, msg.sms_text(r.locale == 'ru' ? 'en' : r.locale).to_lat) if Magti::SEND and (not mobile.blank?)
+      #Magti.send_sms(mobile, msg.sms_text(r.locale == 'ru' ? 'en' : r.locale).to_lat) if Magti::SEND and (not mobile.blank?)
+      Magti.send_sms(mobile, msg.sms_text(r.locale).to_lat) if Magti::SEND and (not mobile.blank?)
       begin
         subject = "TELASI: #{ msg.sms_text(r.locale) }"
         body = msg.email_text(r.locale)
