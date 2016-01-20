46754978ab261882224b5727f03f97c6742e1e20
T <opit@gol.ge>
2016-01-20 17:48:07 +0400

    get active deposit customer instead of first

--- app/models/bs/customer.rb
+++ app/models/bs/customer.rb
@@ -15,9 +15,13 @@ class Bs::Customer < ActiveRecord::Base
   has_many :item_bills,     class_name: 'Bs::ItemBill',      foreign_key: :custkey, order: 'itemkey'
   has_many :accounts,       class_name: 'Bs::Account',       foreign_key: :custkey
   has_one  :note,           class_name: 'Bs::Note',          foreign_key: :notekey
-  has_one  :deposit_customer, class_name: 'Bs::DepositCustomer', foreign_key: :custkey
+  #has_one  :deposit_customer, -> { where status: 0 }, class_name: 'Bs::DepositCustomer', foreign_key: :custkey
   belongs_to :category,     class_name: 'Bs::Custcateg',     foreign_key: :custcatkey
   belongs_to :activity,     class_name: 'Bs::Custcateg',     foreign_key: :activity
+
+  def deposit_customer
+    Bs::DepositCustomer.where(custkey: self.custkey, status: 0).first
+  end
 
   def pre_payment
     Bs::Payment.where('paydate > ? AND custkey = ? AND status = 1', Date.today - 7, self.custkey).inject(0) do |sum, payment|
