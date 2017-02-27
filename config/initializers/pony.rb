86f04e566b6837b5a47471fdf27b450cee75f882
T <opit@gol.ge>
2017-02-27 17:25:04 +0400

    pony configuration

--- config/initializers/pony.rb
+++ config/initializers/pony.rb
@@ -2,11 +2,12 @@
 require 'pony'
 
 smtp_options = {
-  :address        => '92.241.77.33',
-  :port           => '25',
-  #:user_name      => 'support@telasi.ge',
-  #:password       => password,
-  #:authentication => :plain,
+  :address        => 'telmail.telasi.ge',
+  :port           => '587',
+  :enable_starttls_auto => true,
+  :user_name      => '',
+  :password       => password,
+  :authentication => :login,
   #:domain         => "telasi.ge"
 }
 
