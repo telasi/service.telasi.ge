05cb2f68e51941cba5c952b983b1abccb57b5cef
T <opit@gol.ge>
2017-06-22 16:22:08 +0400

    cutter constraints

--- app/models/gnerc/sender.rb
+++ app/models/gnerc/sender.rb
@@ -16,8 +16,10 @@ module Gnerc::Sender
 			cutter.update_attributes!(stage: 2, transaction_number_2: cutter.transaction_number, compare_date_2: item.enter_date_insp)
 
 			if cutter.mainaccount == 1
-				queue = Gnerc::Queue.new(service: Gnerc::Queue::SERVICE, service_id: cutter.id, stage: 2, created_at: Time.now + 4.hours, updated_at: Time.now  + 4.hours)
-				queue.save!
+				if Gnerc::Queue.where(service: Gnerc::Queue::SERVICE, service_id: cutter.id, stage: 2).blank?
+				 queue = Gnerc::Queue.new(service: Gnerc::Queue::SERVICE, service_id: cutter.id, stage: 2, created_at: Time.now + 4.hours, updated_at: Time.now  + 4.hours)
+				 queue.save!
+				end
 			end
 		end
 	end
