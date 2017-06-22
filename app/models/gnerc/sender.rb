80481dc62566c6feba947583725c4e708d407bd7
T <opit@gol.ge>
2017-06-22 15:13:07 +0400

    time fix for cutter queue

--- app/models/gnerc/sender.rb
+++ app/models/gnerc/sender.rb
@@ -16,7 +16,7 @@ module Gnerc::Sender
 			cutter.update_attributes!(stage: 2, transaction_number_2: cutter.transaction_number, compare_date_2: item.enter_date_insp)
 
 			if cutter.mainaccount == 1
-				queue = Gnerc::Queue.new(service: Gnerc::Queue::SERVICE, service_id: cutter.id, stage: 2, created_at: Time.now, updated_at: Time.now)
+				queue = Gnerc::Queue.new(service: Gnerc::Queue::SERVICE, service_id: cutter.id, stage: 2, created_at: Time.now + 4.hours, updated_at: Time.now  + 4.hours)
 				queue.save!
 			end
 		end
