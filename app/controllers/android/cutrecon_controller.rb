5ecdd51a39c6ed73c654b15812f3d40fc583640e
T <opit@gol.ge>
2017-06-23 15:32:08 +0400

    tablet 1970 fix

--- app/controllers/android/cutrecon_controller.rb
+++ app/controllers/android/cutrecon_controller.rb
@@ -88,11 +88,13 @@ class Android::CutreconController < ApplicationController
           item.discrecstatuskey = data['discrecstatuskey'][0] unless data['discrecstatuskey'][0] == 0
         end
 
-        #if data['enter_date'][0][0, 4] == '1970'
-         # item.enter_date_insp = Time.now + 4.hours
-        #else
+        enter_date = data['enter_date'][0][0..3]
+
+        if enter_date[0..3] == '1970' or enter_date[0..3].blank?
+          item.enter_date_insp = Time.now + 4.hours
+        else
           item.enter_date_insp = data['enter_date'][0]
-        #end
+        end
         item.upload_date_insp = Time.now + 4.hours
         item.upload_status = Bs::CutHistory::UPLOAD_STATUS_GNERC if ( item.oper_code == 1 )
         #item.upload_status = Bs::CutHistory::UPLOAD_STATUS_INSPECTOR unless ( item.upload_status == Bs::CutHistory::UPLOAD_STATUS_GNERC )
