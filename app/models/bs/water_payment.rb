33c49bf8ae967c9de44aa9968dfe062758c76b87
T <opit@gol.ge>
2017-06-26 11:56:48 +0400

    trash/water payments status field is integer

--- app/models/bs/water_payment.rb
+++ app/models/bs/water_payment.rb
@@ -2,5 +2,6 @@
 class Bs::WaterPayment < ActiveRecord::Base
   self.table_name  = 'payments.water_payment'
   self.primary_key = :paykey
+  self.set_integer_columns :status
   belongs_to :customer, class_name: 'Bs::Customer', foreign_key: :custkey
 end
