46561c477d191eb37cf3494e6cb5134617904b68
T <opit@gol.ge>
2017-08-30 09:29:41 +0400

    dont send mark_code 2

--- app/models/gnerc/sender.rb
+++ app/models/gnerc/sender.rb
@@ -2,7 +2,7 @@
 module Gnerc::Sender
 
 	def self.stage2(item)
-		return if item.mark_code == 0
+		return if ( item.mark_code in [0, 2] )
 
 		Gnerc::Cutter.transaction do
 			cutter = Gnerc::Cutter.where(transaction_number: item.cr_key).first
